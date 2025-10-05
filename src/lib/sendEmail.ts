b32e47da27e4030be6afbfec45f95d7c28f5a791
bennieking05 <bennie@kanarys.com>
2025-10-05 15:42:21 -0500

    CHANGES TO MAIL

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -2,7 +2,7 @@ import sgMail, { MailDataRequired } from "@sendgrid/mail";
 import dotenv from "dotenv";
 
 dotenv.config();
-const senderEmail = process.env.EMAIL_USER!;
+const senderEmail = process.env.BREVO_SENDER_EMAIL!;
 sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
 function stripHtml(html: string): string {
